ce69de25793804c1088b0b065ce68380bbe7236c
bala11111 <balki.98@gmail.com>
2020-08-02 01:37:23 +0530

    setup static files for layouts and changes

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+app.use(expressLayouts);
 app.use('/',require('./routes'));
+app.set('view engine','ejs');
+app.set('views','./views');
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 
 app.listen(port,function(err)
