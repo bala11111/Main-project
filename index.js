67f35275a4a480fabebd355a91d490681d751f0f
bala11111 <balki.98@gmail.com>
2020-08-03 03:47:15 +0530

    add sign in and signout

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 5000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+const { urlencoded } = require('express');
+
+app.use(urlencoded());
+app.use(cookieParser());
 
 app.use(express.static('./assets'));
 app.use(expressLayouts);
@@ -10,6 +16,7 @@ app.set('view engine','ejs');
 app.set('views','./views');
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
+
 
 
 app.listen(port,function(err)
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 const userController = require('../controllers/user_controller'); 
 
-router.get('/profile',userController.profile);
+router.get('/profile',userController.profile); 
+
+router.get('/sign-in',userController.Signin);
+router.get('/sign-up',userController.Signup);
+
+router.post('/create',userController.create);
 
 module.exports = router;
